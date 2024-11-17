1aaef2757d532d4531ee472e86eb21f0f77ce346
Javier C. <javier.cadagan6@gmail.com>
2024-11-17 03:14:08 -0300

    Limpieza de archivos

--- sistema_reservas.py
+++ sistema_reservas.py
@@ -2,7 +2,6 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import app, db
 from app.models import Paciente, Medico, Horario, Cita
-import tests.US_123 as us123
 
 @app.shell_context_processor
 def make_shell_context():
--- user_story/Paciente/pruebas.py
+++
@@ -1,6 +0,0 @@
-from datetime import date
-from tests import US_45 as us45
-from app.services import db_helper
-from tests.US_45 import buscar_medicos_print
-from app import app
-
