cb77f2d713244cad25bbf33714ac035e53323108
brai44 <braian.urba@gmail.com>
2024-12-03 00:56:17 -0300

    implementacion historial citas web

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from flask import render_template, redirect, url_for
 from app import app
+from app.models import Cita, Paciente, Medico, Especialidad, Horario
+from app.services.db_helper import popular_base_datos, formatear_base_datos
+
 
 # Este archivo define las rutas de la aplicacion
 # se utilizan los decoradores @app.route('/')
@@ -25,4 +28,19 @@ def especialistas():
 def centroayuda():
     return render_template('centroayuda.html')
 
+@app.route('/historial_citas')
 
+def historial_citas():
+    popular_base_datos()
+    print(Horario.query.all())
+    print(Cita.query.all())
+    historial = Cita.get_citas_por_paciente(1)  # Suponiendo que 1 es el ID del paciente
+    for cita in historial:
+        print(f"- Cita ID: {cita.id_cita}")
+        print(f"  Estado: {cita.estado.name}")
+        print(f"  Médico: {cita.medico.nombre} {cita.medico.apellido} - {cita.medico.especialidad}")
+        print(f"  Fecha: {cita.horario.fecha}")
+        print(f"  Bloque Horario: {cita.horario.bloque.name}")
+        print("-" * 30)
+
+    return render_template('historial_citas.html', citas=historial)
